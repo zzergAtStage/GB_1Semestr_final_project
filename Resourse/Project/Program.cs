92dc921e0981aa0bd61cad5ac593f41f8a27b240
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-03-24 16:50:14 +0100

    draft of the code

---
+++ Resourse/Project/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Project;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.Clear();
+       int countOfSymbols = 3;
+       string[] resource = {"hello", "2","world",":-("};
+       System.Console.WriteLine(String.Join(",",resource));
+        System.Console.WriteLine($"[{String.Join(", ",resource)}] => [{String.Join(", ",GetRangedSymbolsWords(resource,countOfSymbols))}]");
+    }
+
+    static string[] GetRangedSymbolsWords(string[] resource,int countOfSymbols){
+        string[] result = new string[resource.Length];
+        int j = 0;
+        for (int i = 0; i < resource.Length; i++)
+        {
+            int tmpValue = result[i].Length;
+            if (tmpValue < countOfSymbols ) {
+                result[j] = result[i];
+            }
+        }
+        return result;
+
+    }
+}
